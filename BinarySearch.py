61a6bd62a8648d684cde78f6551c8978b2a12c2e
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-01-31 09:44:15 +0530

    Binary search in 1d array finished

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,3 +1,4 @@
+# Basic code for Binary search in an array
 def binarySearch(arr, b, e, tar):
 
     while b <= e:
@@ -12,22 +13,37 @@ def binarySearch(arr, b, e, tar):
         else:
             e = mid - 1
     
-    while e>=b:
-        mid=b+(e-b)//2
-        
-        if arr[mid]==tar:
-            return mid
-        
-        elif arr[mid]<tar:
-            e=mid-1
-        
-        else:
-            b=mid+1
-
     return -1
  
 
 list_1=list(map(int,input("Enter list elements: ").split()))
 tar=int(input("Enter the target elemnts: "))
 print( binarySearch(list_1, 0, len(list_1)-1, tar))
+
+#Code for Binary Search (Order Agnostic)
+def BinSearch(arr,tar,s,e):
+    s=0
+    e=len(arr)-1
+    while(s<=e):
+        mid=s+(e-s)//2
+        if(tar==arr[mid]):
+            return mid
+
+        if(arr[s]<arr[e]):
+            if(tar>arr[mid]):
+                s=mid+1
+            else:
+                e=mid-1
+            
+        else:
+            if(tar>arr[mid]):
+                e=mid-1
+            else:
+                s=mid+1
+    return -1
+
+arr=list(map(int,input("Enter the array elements: ").split()))
+tar=int(input("Enter the target: "))
+print(BinSearch(arr,tar,0,len(arr)-1))
+
  
